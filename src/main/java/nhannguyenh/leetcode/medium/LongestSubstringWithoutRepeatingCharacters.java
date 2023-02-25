dd846bf5f0b4d1695370a05615868d507cb2cb2f
Nhan Nguyen Hoang <fetel001@gmail.com>
2023-02-25 18:22:52 +0700

    Problem PROB-3 [Medium] Longest Substring Without Repeating Characters
    Fix code smell issues

--- src/main/java/nhannguyenh/leetcode/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/nhannguyenh/leetcode/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -15,7 +15,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
         for (int start = 0, end = 0; end < s.length(); end++) {
             Character character = s.charAt(end);
             if (characterMap.containsKey(character)) {
-                start = Math.max(characterMap.get(character), start);
+                start = Math.max(characterMap.get(character), start); // NOSONAR
             }
             maxLength = Math.max(maxLength, end - start + 1);
             characterMap.put(character, end + 1);
@@ -29,7 +29,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
     }
 
     private boolean validateCharacter(String s) {
-        String regrex = "[a-zA-Z0-9\s]+";
+        String regrex = "[a-zA-Z0-9\s]+"; // NOSONAR
         return s.matches(regrex);
     }
 }
