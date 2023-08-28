9e7f84c019ea85017b3de602ce43cc73c4d536c4
Nhan Nguyen Hoang <nhoang@investcloud.com>
2023-08-28 17:09:49 +0700

    Problem PROB-9 [Easy] Squares of a Sorted Array
    Add validate methods and unit test

--- src/main/java/nhannguyenh/leetcode/easy/SquaresSortedArray.java
+++ src/main/java/nhannguyenh/leetcode/easy/SquaresSortedArray.java
@@ -5,10 +5,28 @@ import java.util.Arrays;
 public class SquaresSortedArray {
 
     public int[] sortedSquares(int[] nums) {
-        return Arrays.stream(nums)
-                .boxed()
-                .mapToInt(e -> e * e)
-                .sorted()
-                .toArray();
+        if (validate(nums)) {
+            return Arrays.stream(nums)
+                    .boxed()
+                    .mapToInt(this::squaredElement)
+                    .sorted()
+                    .toArray();
+        }
+        throw new IllegalArgumentException("Input array is not valid");
+    }
+
+    private Integer squaredElement(Integer element) {
+        if (validateElement(element)) {
+            return element * element;
+        }
+        throw new IllegalArgumentException(String.format("%d is not valid value", element));
+    }
+
+    private boolean validate(int[] nums) {
+        return nums.length >= 1 && nums.length <= 104;
+    }
+
+    private boolean validateElement(int element) {
+        return element >= -104 && element <= 104;
     }
 }
--- src/test/java/nhannguyenh/test/leetcode/easy/SquaresSortedArrayTest.java
+++ src/test/java/nhannguyenh/test/leetcode/easy/SquaresSortedArrayTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class SquaresSortedArrayTest {
+class SquaresSortedArrayTest {
     private SquaresSortedArray solution;
 
     @BeforeEach
@@ -21,12 +21,30 @@ public class SquaresSortedArrayTest {
     }
 
     @Test
-    public void giveArrayThenReturnSquaresSortedArrayTestCase1() {
+    void givenEmptyArray_thenReturnException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            solution.sortedSquares(new int[]{});
+        });
+
+        assertEquals("Input array is not valid", exception.getMessage());
+    }
+
+    @Test
+    void givenArrayHasLengthGreaterThan104_thenReturnException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            solution.sortedSquares(new int[]{105});
+        });
+
+        assertEquals(String.format("%d is not valid value", 105), exception.getMessage());
+    }
+
+    @Test
+    void giveArray_thenReturnSquaresSortedArrayTestCase1() {
         assertArrayEquals(new int[]{4, 9, 9, 49, 121}, solution.sortedSquares(new int[]{-7, -3, 2, 3, 11}));
     }
 
     @Test
-    public void giveArrayThenReturnSquaresSortedArrayTestCase2() {
+    void giveArray_thenReturnSquaresSortedArrayTestCase2() {
         assertArrayEquals(new int[]{0, 1, 9, 16, 100}, solution.sortedSquares(new int[]{-4, -1, 0, 3, 10}));
     }
 }
